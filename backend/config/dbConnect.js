6f9357a6645ccfc916d74cfbd2a7c043e4e4939b
ojasvik <khuranaojasvi@gmail.com>
2021-01-22 23:13:41 +0530

    database connected

---
+++ backend/config/dbConnect.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const dbConnect = () => {
+    //connect db
+    mongoose.connect("mongodb+srv://okay:zoKjg1xIaZ7CrAlI@cluster0.lrahv.mongodb.net/book-keeping-app", {
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useNewUrlParser: true
+    }).then(() => console.log("db connected")).catch((err) => console.log(err));
+    // zoKjg1xIaZ7CrAlI  okay
+
+}
+
+module.exports = dbConnect;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,32 @@
 const express = require('express');
-
 const app = express();
+require('./config/dbConnect')(); //dbConnect
 
 
+//routes
+//user's routes
+//register
+app.post('/api/users/register',(req,res)=>{
+    res.send('Register route');
+});
+//login
+app.post('/api/users/login',(req,res)=>{
+    res.send('login route');
+});
+//update user
+app.put('/api/users/update',(req,res)=>{
+    res.send('update route');
+});
+app.delete('/api/users/:id',(req,res)=>{
+    res.send('Delete route');
+});
+//fetch Users
+app.get('/api/users',(req,res)=>{
+    res.send('Fetch users');
+});
+
+//Server 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT,()=>{
     console.log(`Server is running ${PORT}`);
 })
